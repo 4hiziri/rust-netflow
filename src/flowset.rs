73df3e5b5d56e3128de3a9799c0665de85e75ac7
4hiziri <meirvg@gmail.com>
2018-05-10 23:25:54 +0900

    Add pasing part

--- src/flowset.rs
+++ src/flowset.rs
@@ -1,6 +1,6 @@
 #![allow(dead_code)]
 
-use field::{Field, Option};
+use field::{NetFlowField, NetFlowOption};
 use nom;
 use byteorder::{BigEndian, ReadBytesExt};
 
@@ -10,7 +10,7 @@ pub struct DataTemplate {
     length: u16,
     template_id: u16,
     field_count: u16,
-    fields: Vec<Field>,
+    fields: Vec<NetFlowField>,
 }
 
 impl DataTemplate {
@@ -32,7 +32,7 @@ pub struct OptionTemplate {
     template_id: u16,
     option_scope_length: u16,
     option_length: u16,
-    options: Vec<Option>,
+    options: Vec<NetFlowOption>,
 }
 
 #[derive(Debug, Clone)]
@@ -82,8 +82,27 @@ pub struct NetFlow9 {
 
 // TODO: use nom to parse payload?
 impl NetFlow9 {
-    pub fn new(payload: &[u8]) {
-        let (payload, ver) = netflow_version(payload).unwrap();
-        println!("Netflow version: {}", ver);
+    pub fn new(payload: &[u8]) -> Option<NetFlow9> {
+        let (payload, version) = netflow_version(payload).unwrap();
+        if version == 9 {
+            let (payload, count) = netflow9_count(payload).unwrap();
+            let (payload, sys_uptime) = netflow9_sys_uptime(payload).unwrap();
+            let (payload, timestamp) = netflow9_timestamp(payload).unwrap();
+            let (payload, flow_sequence) = netflow9_flow_sequence(payload).unwrap();
+            let (payload, flowset_id) = netflow9_flowset_id(payload).unwrap();
+            let flow_sets = payload;
+
+            Some(NetFlow9 {
+                version: version,
+                count: count,
+                sys_uptime: sys_uptime,
+                timestamp: timestamp,
+                flow_sequence: flow_sequence,
+                flowset_id: flowset_id,
+                flow_sets: flow_sets.to_vec(),
+            })
+        } else {
+            None
+        }
     }
 }
