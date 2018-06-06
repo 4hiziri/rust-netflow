3fbec2d92a11a802fba439399695876240be6681
tkgsy <meirvg@gmail.com>
2018-06-06 23:10:40 +0900

    Removed

--- src/flowset/template_set.rs
+++
@@ -1,6 +0,0 @@
-use field::FlowField;
-
-pub trait TemplateSet {
-    fn get_template_len(&self) -> u16;
-    fn parse_dataflow<'a>(&self, payload: &'a [u8]) -> Result<(&'a [u8], Vec<FlowField>), ()>;
-}
